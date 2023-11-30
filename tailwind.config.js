64a3c5b3ca28cd1dbc8ccf236c82bce100148318
Tien Duc <dotienduc201@gmail.com>
2023-11-30 21:58:49 +0700

    project configuration

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      Roboto: ['Roboto', 'serif'],
+      Spicy: ['Spicy Rice', 'serif'],
+    }
+  },
+  plugins: [],
+}
