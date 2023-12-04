fbb989396761e7200308c0e78225bcfee0d89612
Tien Duc <dotienduc201@gmail.com>
2023-12-05 03:35:59 +0700

    Build Login Form

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import HomePage from '@/pages/HomePage';
 import ContactPage from '@/pages/ContactPage';
 import SiteMapPage from '@/pages/SiteMapPage';
+import LoginPage from '@/pages/account/LoginPage';
 
 const routes = [
     {path: '/',component: HomePage },
     {path: '/contact',component: ContactPage },
     {path: '/sitemap',component: SiteMapPage },
+    {path: '/account/login',component: LoginPage },
 ]
 
 export default routes;
